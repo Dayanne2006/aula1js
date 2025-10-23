58561753cc732e983a6826fc127bd865eb0bcce5
Dayanne2006 <s.dayanne2006@gmail.com>
2025-10-23 11:21:18 -0300

    Atividade 1 e 2 Java Script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,25 @@
-alert("Bom Dia:");
+let nome = "Dayanne";
+console.log(nome);
+
+let idade = 18;
+let altura = 1.70;
+console.log("Tenho " + idade + " anos e " + altura + " metros de altura.");
+
+
+let estudando = true;
+
+if (estudando) {
+  console.log("Estou estudando JavaScript!");
+} else {
+  console.log("Não estou estudando no momento.");
+}
+
+    console.log("A soma de 5 + 3 ->");
+    console.log(5 + 3);
+
+    console.log("O resto da divisao de 3/3 ->");
+    console.log(3/3);
+
+    
+    
+
